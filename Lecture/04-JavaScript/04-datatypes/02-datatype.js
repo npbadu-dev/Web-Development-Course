06609d7236ae93b5f2ed65c130b316f606baec79
npbadu-dev <niteshprasadbadu@gmail.com>
2026-03-10 10:27:54 +0545

    datatype example

---
+++ Lecture/04-JavaScript/04-datatypes/02-datatype.js
@@ -0,0 +1,34 @@
+// 1. String
+let name = "John";
+console.log("String:", name);
+
+// 2. Number
+let age = 25;
+console.log("Number:", age);
+
+// 3. BigInt
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log("BigInt:", bigNumber);
+
+// 4. Boolean
+let isStudent = true;
+console.log("Boolean:", isStudent);
+
+// 5. Undefined
+let city;
+console.log("Undefined:", city);
+
+// 6. Null
+let emptyValue = null;
+console.log("Null:", emptyValue);
+
+// 7. Symbol
+let uniqueId = Symbol("id");
+console.log("Symbol:", uniqueId);
+
+// 8. Object
+let person = {
+  name: "John",
+  age: 25,
+};
+console.log("Object:", person);
