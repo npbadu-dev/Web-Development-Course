efa0ac7e93818b7b5dfcb71c6221bcff5422bc9d
npbadu-dev <niteshprasadbadu@gmail.com>
2026-03-24 07:04:54 +0545

    7 challenge done

---
+++ Lecture/04-JavaScript/009-10challenges-array/arrayChallenge.js
@@ -0,0 +1,76 @@
+/* 
+1. Declare an array named `teaFlavors` that contains the strings `"green tea"`, `"black tea"`, and `"oolong tea"`. 
+   Access the first element of the array and store it in a variable named `firstTea`.
+*/
+
+let teaFlavors = ["green tea", "black tea", "oolong tea"];
+let firstTea = teaFlavors[0];
+console.log(firstTea); // Output: "green tea"
+
+/* 
+2. Declare an array named `cities` containing `"London"`, `"Tokyo"`, `"Paris"`, and `"New York"`. 
+   Access the third element in the array and store it in a variable named `favoriteCity`.
+*/
+let cities = ["London", "Tokyo", "Paris", "New York"];
+letfavoriteCity = cities[2];
+console.log(favoriteCity); // Output: "Paris"
+
+/* 
+3. You have an array named `teaTypes` containing `"herbal tea"`, `"white tea"`, and `"masala chai"`. 
+   Change the second element of the array to `"jasmine tea"`.
+*/
+let teaTypes =["herbal tea", "white tea", "masala chai"];
+teaTypes[1] = "jasmine tea";
+console.log(teaTypes); // Output: ["herbal tea", "jasmine tea", "masala chai"]
+
+/* 
+4. Declare an array named `citiesVisited` containing `"Mahendranagar"` and `"Dhangadhi"`. 
+   Add `"Kathmandu"` to the array using the `push` method.
+*/
+
+let citiesVisited = ["Mahendranagar", "Dhangadhi"];
+
+// if we know the length of the array, we can do:
+citiesVisited[2] = "Kathmandu";
+
+// if we don't know the length of the array, we can do:
+console.log(citiesVisited.length); // Output: 2
+citiesVisited[citiesVisited.length] = "Kathmandu";
+
+// the above 2 are the fancy way to add new array element but according to challenges we will use push
+
+citiesVisited.push("Kathmandu"); //add element to the end position
+
+console.log(citiesVisited); // Output: ["Mahendranagar", "Dhangadhi", "Kathmandu"]
+
+
+/* 
+5. You have an array named `teaOrders` with `"chai"`, `"iced tea"`, `"matcha"`, and `"earl grey"`. 
+   Remove the last element of the array using the `pop` method and store it in a variable named `lastOrder`.
+*/
+let teaOrders = ["chai", "iced tea","matcha", "earl gray"]
+teaOrders.pop(); // remove last element, it does not realy matter that how big the array is, it will always remove the last element
+const lastOrder = teaOrders.pop(); // store the removed element in a variable
+console.log(lastOrder);
+
+/* 
+6. You have an array named `popularTeas` containing `"green tea"`, `"oolong tea"`, and `"chai"`. 
+   Create a soft copy of this array named `softCopyTeas`.
+*/
+let popularTeas = ["green tea", "oolong tea", "chai"];
+let softCopyTeas = popularTeas;
+popularTeas.pop(); 
+console.log(softCopyTeas); 
+console.log(popularTeas); 
+
+/* 
+7. You have an array named `topCities` containing `"Berlin"`, `"Singapore"`, and `"New York"`. 
+   Create a hard copy of this array named `hardCopyCities`.
+*/
+let topCities = ["Berlin", "Singapore", "New York"];
+// making a hard copy
+let hardCopyCities = [...topCities];  //method 1
+let hardCopyCities2 = topCities.slice(); // method 2
+
+topCities.pop();
+console.log(hardCopyCities);
